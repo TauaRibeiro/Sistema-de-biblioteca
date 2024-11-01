a7e7576f15dec80c56b531dec0447dda8465cbee
Roque Francisco <165854231+roqueneto12@users.noreply.github.com>
2024-11-01 20:14:41 -0300

    Código base

---
+++ sistema-biblioteca/src/pacoteLivro/Livro.java
@@ -0,0 +1,57 @@
+
+package pacoteLivro;
+
+public class Livro {
+    // Declaração de variáveis de instância privadas
+    private String titulo;
+    private String autor;
+    private int ano;
+    private String genero;
+
+    // Construtor da classe que inicializa as variáveis de instância
+    public Livro(String titulo, String autor, int ano, String genero) {
+        this.titulo = titulo;
+        this.autor = autor;  
+        this.ano = ano; 
+        this.genero = genero;
+    }
+
+    // Métodos getters e setters para acessar e modificar os atributos privados
+
+    public String getTitulo() {
+        return titulo; // Retorna o título do cadastro
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo; // Define um novo valor para o título
+    }
+
+    public String getAutor() {
+        return autor; // Retorna o autor do cadastro
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor; // Define um novo valor para o autor
+    }
+
+    public int getAno() {
+        return ano; // Retorna o ano do cadastro
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano; // Define um novo valor para o ano
+    }
+
+    public String getGenero() {
+        return genero; // Retorna o gênero do cadastro
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero; // Define um novo valor para o gênero
+    }
+}
+
+    
+
+
+
