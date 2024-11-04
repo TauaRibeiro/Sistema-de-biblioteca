6139e095d883821866265f715452015fff9cd7d8
Aluno SENAI LAURO <aluno@SENAI-LAURO.LOCAL>
2024-11-04 20:39:47 -0300

    Terminando classe pai Usuario

--- sistema-biblioteca/src/pacoteUsuario/Usuario.java
+++ sistema-biblioteca/src/pacoteUsuario/Usuario.java
@@ -66,10 +66,47 @@ public class Usuario {
         StringBuilder resultado = new StringBuilder();
         
         for(Usuario u:lista_usuarios){
-            resultado.append(u.toString());
+            resultado.append(u.toString()).append('\n');
         }
         
         return resultado.toString();
     }
     
+    public static String editarUsuario(int id_usuario, Usuario usuario_editado){
+        if(lista_usuarios.isEmpty()){
+            return "Não há usuários cadastrados...";
+        }
+        
+        for(Usuario u:lista_usuarios){
+            if(u.id == id_usuario){
+                if(usuario_editado.nome != null){
+                    u.nome = usuario_editado.nome;
+                }
+                
+                if(usuario_editado.senha != null){
+                    u.senha = usuario_editado.senha;
+                }
+                
+                return "Usuário editado com sucesso!!";
+            }
+        }
+        
+        return "Usuário não encontrado";
+    }
+    
+    public static String excluirUsuario(int id_usuario){
+        if(lista_usuarios.isEmpty()){
+            return "Não há usuário cadastrados...";
+        }
+        
+        for(Usuario u:lista_usuarios){
+            if(u.id == id_usuario){
+                lista_usuarios.remove(u);
+                
+                return "Usuário removido com sucesso!!";
+            }
+        }
+        
+        return "Usuário não encontrado...";
+    }
 }
